a09d88c33fe522d06eb3b6447cf82c4c1e847c58
Jhan Frank <jhnfrankz@gmail.com>
2023-06-18 11:14:43 -0500

    Implemented custom hook useUsers

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,57 +1,17 @@
-import { useReducer, useState } from "react";
 import { UserForm } from "./components/UserForm";
 import { UsersList } from "./components/UsersList";
-import { usersReducer } from "./reducers/usersReducer";
-
-const initialUsers = [
-    {
-        id: 1,
-        username: 'pepe',
-        password: '12345',
-        email: 'pepe@correo.com',
-    }
-];
-
-const initialUserForm = {
-    id: 0,
-    username: '',
-    password: '',
-    email: '',
-};
+import { useUsers } from "./hooks/useUsers";
 
 export const UsersApp = () => {
 
-    const [users, dispatch] = useReducer(usersReducer, initialUsers);
-    const [userSelected, setUserSelected] = useState(initialUserForm);
-
-    const handlerAddUser = (user) => {
-        // console.log(user);
-        let type;
-
-        if (user.id === 0) {
-            type = 'addUser';
-        } else {
-            type = 'updateUser';
-        }
-
-        dispatch({
-            type,
-            payload: user,
-        });
-    };
-
-    const handlerRemoveUser = (id) => {
-        // console.log(id);
-        dispatch({
-            type: 'removeUser',
-            payload: id,
-        });
-    };
-
-    const handlerUserSelectedForm = (user) => {
-        // console.log(user);
-        setUserSelected({ ...user });
-    };
+    const {
+        users,
+        userSelected,
+        initialUserForm,
+        handlerAddUser,
+        handlerRemoveUser,
+        handlerUserSelectedForm,
+    } = useUsers();
 
     return (
         <div className="container my-4">
