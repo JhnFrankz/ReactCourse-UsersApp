43850a1f597a56e7ef31e5f972eb3a22def791d7
Jhan Frank <jhnfrankz@gmail.com>
2023-06-18 22:32:09 -0500

    Implementing Login Form events

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+const initialLoginForm = {
+    username: '',
+    password: '',
+};
 
 export const LoginPage = () => {
+
+    const [loginForm, setLoginForm] = useState(initialLoginForm);
+    const { username, password } = loginForm;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setLoginForm({
+            ...loginForm, // para que no se pierda el valor de la otra propiedad, por ejemplo si se escribe en el input de password, no se pierde el valor de username
+            [name]: value, // para que se actualice el valor de la propiedad que se esta escribiendo
+        });
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (!username || !password) {
+            Swal.fire('Error de validación', 'Username y password requeridos', 'error');
+        }
+
+        // implementamos el login
+        if (username === 'admin' && password === '12345') {
+            // handlerLogin();
+        } else {
+            Swal.fire('Error Login', 'Username o password inválidos', 'error');
+        }
+
+        setLoginForm(initialLoginForm);
+    };
 
     return (
         <>
@@ -10,17 +44,21 @@ export const LoginPage = () => {
                             <h5 className="modal-title">Login Page</h5>
                         </div>
 
-                        <form>
+                        <form onSubmit={onSubmit}>
                             <div className="modal-body">
                                 <input
                                     className="form-control my-3 w-75"
                                     placeholder="Username"
-                                    name="username" />
+                                    name="username"
+                                    value={username}
+                                    onChange={onInputChange} />
                                 <input
                                     className="form-control my-3 w-75"
                                     placeholder="Password"
                                     type="password"
-                                    name="password" />
+                                    name="password"
+                                    value={password}
+                                    onChange={onInputChange} />
                             </div>
                             <div className="modal-footer">
                                 <button
