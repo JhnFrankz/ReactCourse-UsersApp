93ad807184cb2db4f8b76f2ea9524e6594e30ec1
Jhan Frank <jhnfrankz@gmail.com>
2023-06-23 10:03:53 -0500

    Creating context for users

---
+++ src/context/UserProvider.jsx
@@ -0,0 +1,38 @@
+import { UserContext } from "./UserContext";
+
+// pasamos el children para que se pueda renderizar el componente que se le pase como hijo
+export const UserProvider = ({ children }) => {
+
+    const {
+        users,
+        userSelected,
+        initialUserForm,
+        visibleForm,
+        handlerAddUser,
+        handlerRemoveUser,
+        handlerUserSelectedForm,
+        handlerOpenForm,
+        handlerCloseForm,
+    } = useUsers();
+    // el useUsers() solamente se ejecutará una vez
+
+    // children es el componente que se le pase como hijo
+    // los valores que se pasen en value van a estar disponibles para todos los componentes que estén dentro de UserProvider como una especie de repositorio global
+    return (
+        <UserContext.Provider value={
+            {
+                users,
+                userSelected,
+                initialUserForm,
+                visibleForm,
+                handlerAddUser,
+                handlerRemoveUser,
+                handlerUserSelectedForm,
+                handlerOpenForm,
+                handlerCloseForm,
+            }
+        }>
+            {children}
+        </UserContext.Provider>
+    );
+};
--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -3,48 +3,40 @@ import { UsersPage } from "../pages/UsersPage";
 import { Navbar } from "../components/layout/Navbar";
 import { RegisterPage } from "../pages/RegisterPage";
 import { useUsers } from "../hooks/useUsers";
+import { UserProvider } from "../context/UserProvider";
 
 export const UserRoutes = ({ login, handlerLogout }) => {
 
-    const {
-        users,
-        userSelected,
-        initialUserForm,
-        visibleForm,
-        handlerAddUser,
-        handlerRemoveUser,
-        handlerUserSelectedForm,
-        handlerOpenForm,
-        handlerCloseForm,
-    } = useUsers();
-
+    // todos los componentes que estén dentro de UserProvider van a tener acceso a los valores que se pasen en value
     return (
         <>
-            <Navbar login={login} handlerLogout={handlerLogout} />
+            <UserProvider>
+                <Navbar login={login} handlerLogout={handlerLogout} />
 
-            <Routes>
-                <Route path="users" element={<UsersPage
-                    users={users}
-                    userSelected={userSelected}
-                    initialUserForm={initialUserForm}
-                    visibleForm={visibleForm}
-                    handlerAddUser={handlerAddUser}
-                    handlerRemoveUser={handlerRemoveUser}
-                    handlerUserSelectedForm={handlerUserSelectedForm}
-                    handlerOpenForm={handlerOpenForm}
-                    handlerCloseForm={handlerCloseForm} />} />
+                <Routes>
+                    <Route path="users" element={<UsersPage
+                        users={users}
+                        userSelected={userSelected}
+                        initialUserForm={initialUserForm}
+                        visibleForm={visibleForm}
+                        handlerAddUser={handlerAddUser}
+                        handlerRemoveUser={handlerRemoveUser}
+                        handlerUserSelectedForm={handlerUserSelectedForm}
+                        handlerOpenForm={handlerOpenForm}
+                        handlerCloseForm={handlerCloseForm} />} />
 
-                <Route path="users/register" element={<RegisterPage
-                    handlerAddUser={handlerAddUser}
-                    initialUserForm={initialUserForm} />} />
+                    <Route path="users/register" element={<RegisterPage
+                        handlerAddUser={handlerAddUser}
+                        initialUserForm={initialUserForm} />} />
 
-                <Route path="users/edit/:id" element={<RegisterPage
-                    users={users}
-                    handlerAddUser={handlerAddUser}
-                    initialUserForm={initialUserForm} />} />
+                    <Route path="users/edit/:id" element={<RegisterPage
+                        users={users}
+                        handlerAddUser={handlerAddUser}
+                        initialUserForm={initialUserForm} />} />
 
-                <Route path="/" element={<Navigate to="/users" />} />
-            </Routes>
+                    <Route path="/" element={<Navigate to="/users" />} />
+                </Routes>
+            </UserProvider>
         </>
     );
 };
