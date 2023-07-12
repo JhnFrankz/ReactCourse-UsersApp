16c447a0203d9123fba53535926daffe1aa28fc0
Jhan Frank <jhnfrankz@gmail.com>
2023-07-12 10:34:38 -0500

    Managing Token JWT in useAuth hook and loginReducer

--- src/auth/hooks/useAuth.js
+++ src/auth/hooks/useAuth.js
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 // si existe el login en el sessionStorage, se obtiene, sino se crea un objeto con los valores por defecto
 const initialLogin = JSON.parse(sessionStorage.getItem('login')) || {
     isAuth: false,
+    isAdmin: false,
     user: undefined,
 };
 
@@ -15,28 +16,40 @@ export const useAuth = () => {
     const [login, dispatch] = useReducer(loginReducer, initialLogin);
     const navigate = useNavigate();
 
+    // handler para el login, se llama desde el componente LoginPage
     const handlerLogin = async ({ username, password }) => {
-        
 
         try {
             // response contiene el data que contiene el token
             const response = await loginUser({ username, password });
             const token = response.data.token;
-            // este objeto simula la respuesta del backend
-            const user = { username: response.data.username };
+            // se decodifica el token para obtener los claims (payload)
+            const claims = JSON.parse(window.atob(token.split('.')[1]));
+            console.log(claims);
+            const user = { username: claims.username };
+
             dispatch({
                 type: 'login',
-                payload: user,
+                payload: { user, isAdmin: claims.isAdmin },
             });
 
             sessionStorage.setItem('login', JSON.stringify({
                 isAuth: true,
+                isAdmin: claims.isAdmin,
                 user,
             }));
+            // guardamos el token para luego enviarlo en el header de las peticiones
+            sessionStorage.setItem('token', `Bearer ${token}`);
 
             navigate('/users');
         } catch (error) {
-            Swal.fire('Error Login', 'Username o password inválidos', 'error');
+            if (error.response?.status === 401) {
+                Swal.fire('Error Login', 'Username o password inválidos', 'error');
+            } else if (error.response?.status === 403) {
+                Swal.fire('Error Login', 'No tiene acceso al recurso o permisos', 'error');
+            } else {
+                throw error;
+            }
         }
     };
 
@@ -47,7 +60,9 @@ export const useAuth = () => {
             type: 'logout',
         });
 
+        sessionStorage.removeItem('token');
         sessionStorage.removeItem('login');
+        sessionStorage.clear();
     };
 
     return {
