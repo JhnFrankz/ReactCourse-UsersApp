de53a6bf6dd9f2e171a0044d2388766703730295
Jhan Frank <jhnfrankz@gmail.com>
2023-07-13 10:43:33 -0500

    Adding Redux

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// aqui se maneja el almacenamiento de la aplicacion
+export const store = configureStore({
+    reducer: {
+        
+    },
+});
